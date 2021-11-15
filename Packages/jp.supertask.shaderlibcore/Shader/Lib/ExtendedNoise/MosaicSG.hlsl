07d456be1fe46606ac7c7bf0774dff9a972960ad
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2021-11-15 12:14:52 +0900

    Update

---
+++ Packages/jp.supertask.shaderlibcore/Shader/Lib/ExtendedNoise/MosaicSG.hlsl
@@ -0,0 +1,26 @@
+﻿
+#ifndef IMAGE_EFFECT_MOSAIC
+#define IMAGE_EFFECT_MOSAIC
+
+void MosaicUV_float(
+	float2 uv,
+	float mosaicScale,
+	out float2 mosaicUV)
+{
+	float2 delta = saturate(mosaicScale / _ScreenSize.xy);
+	mosaicUV = (floor(uv / delta) + 0.5) * delta;
+}
+
+void MosaicCircle_float(
+	float2 uv,
+	float mosaicScale,
+	out float circle)
+{
+	float2 delta = saturate(mosaicScale / _ScreenSize.xy);
+	float2 uv2 = frac(uv / delta);
+	float len = 0.5 - length(uv2 - 0.5);
+	circle = smoothstep(0.0, 0.05, len);
+}
+
+
+#endif
---
+++ Packages/jp.supertask.shaderlibcore/Shader/Lib/Noise/GlitchSG.hlsl
@@ -0,0 +1,16 @@
+﻿#ifndef GLITCH_UTIL
+#define GLITCH_UTIL
+
+void BasicGlitch_float(float2 uv, int samples, out float4 res)
+{
+    float4 c = float4(uv,0,1);
+    float4 e;
+    for (int i = 0; i < samples; i++)
+    {
+        e = floor(c);
+        res += sin(e * e.yxyx + e * (_Time/10));
+        c *= 3.0;
+    }
+}
+
+#endif
